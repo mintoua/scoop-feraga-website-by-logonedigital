fd756f94aa2a23a928fe327138f1c3e06d68b7cb
NGUE EMMANUEL <ngueemmanuel.prof@gmail.com>
2022-08-02 19:32:26 +0100

    easyadmin actions restricting

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,6 +3,14 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class UserCrudController extends AbstractCrudController
@@ -13,14 +21,32 @@ class UserCrudController extends AbstractCrudController
         return User::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('firstname')->setLabel('Nom'),
+            TextField::new('lastname')->setLabel('Prenom'),
+            EmailField::new('email'),
+            ArrayField::new('roles'),
+            // ChoiceField::new('roles')->setChoices([
+            //     'User' => 'ROLE_USER',
+            //     'Employes' => 'ROLE_ADMIN',
+            //     'Administrateur' => 'ROLE_SUPER_ADMIN'
+            // ])->autocomplete()
+            // ->hideOnIndex(),
+            BooleanField::new('rgpd'),
+            BooleanField::new('isVirified')->setLabel('compte active'),
         ];
     }
-    */
+    
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->add(Crud::PAGE_EDIT, Action::SAVE_AND_ADD_ANOTHER)
+        ;
+    }
+    
 }
