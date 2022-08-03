ea3c6ef317bf584ddd49b700d1696ef79a4d5c4c
NGUE EMMANUEL <ngueemmanuel.prof@gmail.com>
2022-08-03 07:36:57 +0100

    usercrud edited

--- src/Controller/Admin/ContactCrudController.php
+++ src/Controller/Admin/ContactCrudController.php
@@ -3,7 +3,12 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Contact;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 
 class ContactCrudController extends AbstractCrudController
 {
@@ -12,14 +17,20 @@ class ContactCrudController extends AbstractCrudController
         return Contact::class;
     }
 
-    /*
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+        ->setPermission(Action::DELETE, 'ROLE_SUPER_ADMIN')
+        ->disable(Action::EDIT);
+    }
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            TextField::new('nom'),
+            TextField::new('prenom'),
+            EmailField::new('email'),
+            TextEditorField::new('msg'),
         ];
     }
-    */
+    
 }
--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -6,11 +6,13 @@ use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Filters;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Filter\BooleanFilter;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 
 class UserCrudController extends AbstractCrudController
@@ -28,18 +30,25 @@ class UserCrudController extends AbstractCrudController
             TextField::new('firstname')->setLabel('Nom'),
             TextField::new('lastname')->setLabel('Prenom'),
             EmailField::new('email'),
-            ArrayField::new('roles'),
-            // ChoiceField::new('roles')->setChoices([
-            //     'User' => 'ROLE_USER',
-            //     'Employes' => 'ROLE_ADMIN',
-            //     'Administrateur' => 'ROLE_SUPER_ADMIN'
-            // ])->autocomplete()
-            // ->hideOnIndex(),
+            ChoiceField::new('roles')->setChoices([
+                'User' => 'ROLE_USER',
+                'Employes' => 'ROLE_ADMIN',
+                'Administrateur' => 'ROLE_SUPER_ADMIN'
+            ])->allowMultipleChoices(),
             BooleanField::new('rgpd'),
-            BooleanField::new('isVirified')->setLabel('compte active'),
+            BooleanField::new('isVirified')->setLabel('activer le compte'),
         ];
     }
     
+    public function configureFilters(Filters $filters): Filters
+    {
+        return $filters
+            ->add('firstname')
+            ->add('lastname')
+            ->add('email')
+            ->add(BooleanFilter::new('isVirified'))
+        ;
+    }
 
     public function configureActions(Actions $actions): Actions
     {
@@ -47,6 +56,14 @@ class UserCrudController extends AbstractCrudController
             ->add(Crud::PAGE_INDEX, Action::DETAIL)
             ->add(Crud::PAGE_EDIT, Action::SAVE_AND_ADD_ANOTHER)
         ;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setSearchFields(['lastname', 'firstname', 'email'])
+            
+            ->setAutofocusSearch();
     }
     
 }
