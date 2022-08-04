9562949fa630fb494f3719eb6132ee826695efe1
mintoua <toupkandi.mintoua@esprit.tn>
2022-08-04 22:13:37 +0100

    generate pdf for order details

--- src/Controller/Admin/CarrierCrudController.php
+++ src/Controller/Admin/CarrierCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Carrier;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
@@ -15,13 +16,19 @@ class CarrierCrudController extends AbstractCrudController
         return Carrier::class;
     }
 
+    public function configureCrud ( Crud $crud ) : Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular ('Transporteur')
+            ->setEntityLabelInPlural  ('Transporteurs');
+    }
 
     public function configureFields(string $pageName): iterable
     {
         return [
-            TextField::new('name'),
-            IntegerField::new('price'),
-            TextEditorField::new ('description')
+            TextField::new('name','Nom'),
+            IntegerField::new('price','Prix'),
+            TextEditorField::new ('description','Description')
         ];
     }
 
